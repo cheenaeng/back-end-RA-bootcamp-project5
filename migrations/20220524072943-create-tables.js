f40306eb32e5ca3a36110629b0ed293c94ed4dd6
cheenaeng <cheena94sing@gmail.com>
2022-05-24 15:39:59 +0800

    set up migraion table

---
+++ migrations/20220524072943-create-tables.js
@@ -0,0 +1,82 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      display_name: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.createTable('coffees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      milk: {
+        type: Sequelize.JSON,
+      },
+      sugar: {
+        type: Sequelize.STRING,
+      },
+      concentration: {
+        type: Sequelize.DECIMAL,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.createTable('user_coffees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      coffee_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'coffees',
+          key: 'id',
+        },
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('user_coffees');
+    await queryInterface.dropTable('coffees');
+    await queryInterface.dropTable('users');
+  },
+};
