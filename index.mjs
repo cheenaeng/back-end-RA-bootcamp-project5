9083e56de27307701e43bbe9aa4df2ebac88c29d
cheenaeng <cheena94sing@gmail.com>
2022-07-02 15:49:12 +0800

    changed port details

--- index.mjs
+++ index.mjs
@@ -29,6 +29,6 @@ bindRoutes(app);
 
 // Set Express to listen on the given port
 // const PORT = process.env.PORT || 3004;
-const PORT = process.argv[2];
+const PORT = process.env.PORT || 3004;
 
 app.listen(PORT);
